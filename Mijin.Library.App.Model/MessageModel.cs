c15c6c34e8866d5ddc7dfafb814e421139a7df0b
zhuyi <1047606598@qq.com>
2021-02-11 19:05:08 +0800

    修改MessageModel采用继承方式

--- Mijin.Library.App.Model/MessageModel.cs
+++ Mijin.Library.App.Model/MessageModel.cs
@@ -1,58 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using Util.Helpers;
+using Util;
 
-namespace Mijin.Library.App.Model.Model
+namespace Mijin.Library.App.Model
 {
     /// <summary>
     /// 通用返回信息类
     /// </summary>
-    public class MessageModel<T>
+    public class MessageModel<T> : baseMessageModel
     {
+        public MessageModel(baseMessageModel @base) : base(@base)
+        {
+
+        }
         public MessageModel()
         {
 
         }
-        /// <summary>
-        /// 该构造函数最好还是重新赋值response
-        /// </summary>
-        /// <param name="messageModel"></param>
-        public MessageModel(dynamic messageModel)
-        {
-            status = messageModel.status;
-            success = messageModel.success;
-            msg = messageModel.msg;
-            devMsg = messageModel.devMsg;
+        ///// <summary>
+        ///// 该构造函数最好还是重新赋值response
+        ///// </summary>
+        ///// <param name="messageModel"></param>
+        //public MessageModel(dynamic messageModel)
+        //{
+        //    status = messageModel.status;
+        //    success = messageModel.success;
+        //    msg = messageModel.msg;
+        //    devMsg = messageModel.devMsg;
 
-            try
-            {
-                //response = (T)messageModel.response;
-                response = Json.ToObject<T>(Json.ToJson(messageModel.response));
-            }
-            catch (Exception)
-            {
-            }
-        }
+        //    try
+        //    {
+        //        //response = (T)messageModel.response;
+        //        response = Json.ToObject<T>(Json.ToJson(messageModel.response));
+        //    }
+        //    catch (Exception)
+        //    {
+        //    }
+        //}
 
-        /// <summary>
-        /// 状态码
-        /// </summary>
-        public int status { get; set; } = 200;
-        /// <summary>
-        /// 操作是否成功
-        /// </summary>
-        public bool success { get; set; } = false;
-        /// <summary>
-        /// 返回信息
-        /// </summary>
-        public string msg { get; set; } = "服务器异常";
-
-        /// <summary>
-        /// 调试信息
-        /// </summary>
-        public string devMsg { get; set; }
-        /// <summary>
         /// 返回数据集合
         /// </summary>
         public T response { get; set; }
---
+++ Mijin.Library.App.Model/baseMessageModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mijin.Library.App.Model
+{
+    public class baseMessageModel
+    {
+        public baseMessageModel()
+        {
+        }
+
+        public baseMessageModel(baseMessageModel @base)
+        {
+            status = @base.status;
+            success = @base.success;
+            msg = @base.msg;
+            devMsg = @base.devMsg;
+        }
+
+        /// <summary>
+        /// 状态码
+        /// </summary>
+        public int status { get; set; } = 200;
+        /// <summary>
+        /// 操作是否成功
+        /// </summary>
+        public bool success { get; set; } = false;
+        /// <summary>
+        /// 返回信息
+        /// </summary>
+        public string msg { get; set; } = "服务器异常";
+
+        /// <summary>
+        /// 调试信息
+        /// </summary>
+        public string devMsg { get; set; }
+    }
+}
