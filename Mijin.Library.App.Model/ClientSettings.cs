ac4386b1ebca650d5ce7b2f0e4bd34b5fd612b48
zhuyi <1047606598@qq.com>
2021-02-12 18:19:27 +0800

    添加图书馆窗体设置类

---
+++ Mijin.Library.App.Model/ClientSettings.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Util;
+using Util.Helpers;
+
+namespace Mijin.Library.App.Model
+{
+    
+    /// <summary>
+    /// 客户端设置
+    /// </summary>
+    public class ClientSettings : baseClientSettings
+    {
+        public ClientSettings()
+        {
+            // 获取base类的属性
+            var settings = ReadToBaseModel();
+
+            // 有值就反射赋值
+            if (settings != null)
+            {
+                var type = typeof(baseClientSettings);
+                foreach (var propertyInfo in type.GetProperties())
+                {
+                    propertyInfo.SetValue(this, propertyInfo.GetValue(settings));
+                }
+            }
+        }
+
+        /// <summary>
+        /// 写入到本地文件 appsettings.json
+        /// </summary>
+        /// Exception 写入失败
+        /// <returns></returns>
+        public void Write()
+        {
+           FileHelper.WriteFile("./appsettings.json", Json.ToJson(this),Encoding.UTF8);
+        }
+
+        /// <summary>
+        /// 读本地文件 appsettings.json ,如果没有则返回null
+        /// </summary>
+        /// <returns></returns>
+        private baseClientSettings ReadToBaseModel()
+        {
+            // 是否含有 appsettings.json 文件
+            if (File.Exists("./appsettings.json"))
+            {
+                try
+                {
+                    var settingsFile = FileHelper.ReadFile("./appsettings.json",Encoding.UTF8);
+                    var data =  Json.ToObject<baseClientSettings>(settingsFile);
+                    return data;
+                }
+                catch (Exception e)
+                {
+                    FileHelper.FileAdd("./appsettings.json", "\r\n\r\n" + "//ErrorMsg：序列化失败，请检查Json 格式");
+                    return null;
+                }
+                //读取并序列化appsettings
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+    }
+}
+
---
+++ Mijin.Library.App.Model/baseClientSettings.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mijin.Library.App.Model
+{
+    public class baseClientSettings
+    {
+        /// <summary>
+        /// 后台管理打开的URL
+        /// </summary>
+        public string LibraryManageUrl { get; set; }
+        /// <summary>
+        /// 自助借阅打开的URL
+        /// </summary>
+        public string ReaderActionUrl { get; set; }
+
+        /// <summary>
+        /// 此字段若有值，则跳过选择窗口直接打开页面窗口
+        /// </summary>
+        public string NoSelectOpenUrl { get; set; }
+
+        /// <summary>
+        /// 窗口高
+        /// </summary>
+        public int WindowHeight { get; set; }
+
+        /// <summary>
+        /// 窗口宽
+        /// </summary>
+        public int WindowWidth { get; set; }
+
+        /// <summary>
+        /// 显示窗口标题栏
+        /// </summary>
+        public bool ShowWindowTitleBar { get; set; } = true;
+
+        /// <summary>
+        /// 可否手动调整窗口宽高
+        /// </summary>
+        public bool CanResize { get; set; } = true;
+
+        /// <summary>
+        /// 门禁Url
+        /// </summary>
+        public List<string> DoorUrls { get; set; }
+
+        /// <summary>
+        /// 每次退出清空浏览器缓存
+        /// </summary>
+        public bool OnExitClearWebCookie { get; set; }
+
+        /// <summary>
+        /// 开发者模式
+        /// </summary>
+        public bool IsDev { get; set; }
+
+    }
+}
