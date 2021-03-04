b39debacf12f557592f883a7f295b89c7f477251
zhuyi <1047606598@qq.com>
2021-03-04 16:02:54 +0800

    添加driver单元测试

---
+++ Mijin.Library.App.Tests/Driver/HFReader_Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Mijin.Library.App.Tests.Driver
+{
+    public class HFReader_Test
+    {
+        [Fact]
+        public 
+    }
+}
