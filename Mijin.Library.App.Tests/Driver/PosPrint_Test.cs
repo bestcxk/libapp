d2999c7e31b3bfa4f5185523a98d314fbe923e31
yi zhu <1047606598@qq.com>
2021-03-17 10:14:52 +0800

    小票打印机单元测试，未完成

--- Mijin.Library.App.Tests/Driver/PosPrint_Test.cs
+++ Mijin.Library.App.Tests/Driver/PosPrint_Test.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿using Mijin.Library.App.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Mijin.Library.App.Tests.Driver
 {
-    class PosPrint_Test
+    public class PosPrint_Test
     {
+        IPosPrint _posPrint;
+        public PosPrint_Test()
+        {
+            _posPrint = new MyPosPrint();
+        }
+
+        [Fact]
+        public void Print_Test()
+        {
+            var res = _posPrint.Print(new PrintInfo()
+            {
+                Action = ActionType.借阅,
+                CreateTime = DateTime.Now,
+                BookInfos = new List<BookInfo>() { 
+                    new BookInfo(){ 
+                        Title = "test",
+                        Isbn = "123456789112"
+                    }
+                },
+                UserName = "test",
+                UserCardNo = "test",
+                UserIdentity = "test"
+            });
+
+            Assert.True(res.success);
+        }
     }
+
+
+
 }
