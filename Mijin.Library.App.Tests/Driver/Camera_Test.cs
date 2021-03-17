576af341c71509d65e4a59ac8b0ad82c1cc500ae
yi zhu <1047606598@qq.com>
2021-03-17 15:33:47 +0800

    添加camera单元测试

--- Mijin.Library.App.Tests/Driver/Camera_Test.cs
+++ Mijin.Library.App.Tests/Driver/Camera_Test.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using Mijin.Library.App.Driver.Drivers.Camera;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Mijin.Library.App.Tests.Driver
 {
-    class Camera_Test
+    public class Camera_Test
     {
+        ICamera _camera;
+        public Camera_Test()
+        {
+            _camera = new Camera();
+        }
+
+        [Fact]
+        public async void TestCamera()
+        {
+            _camera.OnCameraGetImage += _camera_OnCameraGetImage;
+            var open = _camera.OpenCamera();
+            await Task.Delay(3000);
+            var close = _camera.CloseCamera();
+        }
+
+        private void _camera_OnCameraGetImage(Model.WebViewSendModel<string> obj)
+        {
+            
+        }
     }
+
+    
 }
