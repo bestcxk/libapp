114b4a886471d99ddcd6643b660bae14b376e7b8
yi zhu <1047606598@qq.com>
2021-03-24 17:18:25 +0800

    修复命名空间

--- Mijin.Library.App.Driver/Drivers/Camera/Camera.cs
+++ Mijin.Library.App.Driver/Drivers/Camera/Camera.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Mijin.Library.App.Driver.Drivers.Camera
+namespace Mijin.Library.App.Driver
 {
     public class Camera : ICamera
     {
