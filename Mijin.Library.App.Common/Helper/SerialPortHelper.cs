c5e94690476cd69f25e0d8891cce2b377896a6df
zhuyi <1047606598@qq.com>
2021-03-25 17:32:35 +0800

    添加串口通讯帮助类

---
+++ Mijin.Library.App.Common/Helper/SerialPortHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Util.Helpers
+{
+    public class SerialPortHelper
+    {
+        /// <summary>
+        /// BCC和校验代码
+        /// </summary>
+        /// <param name="data">需要校验的数据包</param>
+        /// <returns></returns>
+        public static byte Get_CheckXor(IEnumerable<byte> data)
+        {
+            byte CheckCode = 0;
+            foreach (var item in data)
+            {
+                CheckCode ^= item;
+            }
+            return CheckCode;
+        }
+    }
+}
