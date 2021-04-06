289c6c8cabcb42679264c1a44fe781e2dbfb52bc
yi zhu <1047606598@qq.com>
2021-04-06 19:38:05 +0800

    修改app文件名

---
+++ Mijin.Library.App/Filters/GlobalExceptionsFilter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+using Util.Logs;
+using Util.Logs.Extensions;
+
+namespace Mijin.Library.App.Filters
+{
+    /// <summary>
+    /// 全局异常处理
+    /// </summary>
+    public class GlobalExceptionsFilter
+    {
+        /// <summary>
+        /// 触发异常时，写日志 (捕获不到 非 await 的子线程异常)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public static void OnException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+            e.Exception.Log(Log.GetLog().Caption("全局异常处理"));
+        }
+    }
+}
--- Mijin.Library.WinApp/DependInstaller/readme.txt
+++
@@ -1,2 +0,0 @@
-MicrosoftEdgeWebview2Setup.exe  ： webview2 运行时 安装包
-Windows6.1-KB3063858-x64.msu   ： win7需要安装该包
