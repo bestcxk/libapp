b6a373c70ec31987b1c35121d22ef616eadeadaa
yi zhu <1047606598@qq.com>
2021-05-13 14:34:42 +0800

    添加16进制转换函数

--- Mijin.Library.App.Common/Helper/SerialPortHelper.cs
+++ Mijin.Library.App.Common/Helper/SerialPortHelper.cs
@@ -22,5 +22,23 @@ namespace Util.Helpers
             }
             return CheckCode;
         }
+
+        #region  16进制字符串到数组之间的相互转换
+        public static byte[] HexStringToByteArray(string s)
+        {
+            s = s.Replace(" ", "");
+            byte[] buffer = new byte[s.Length / 2];
+            for (int i = 0; i < s.Length; i += 2)
+                buffer[i / 2] = (byte)System.Convert.ToByte(s.Substring(i, 2), 16);
+            return buffer;
+        }
+        public static string ByteArrayToHexString(byte[] data)
+        {
+            StringBuilder sb = new StringBuilder(data.Length * 3);
+            foreach (byte b in data)
+                sb.Append(System.Convert.ToString(b, 16).PadLeft(2, '0').PadRight(3, ' '));
+            return sb.ToString().ToUpper();
+        }
+        #endregion
     }
 }
