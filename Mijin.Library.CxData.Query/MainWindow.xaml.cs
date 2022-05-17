3905e2a76380b5d47c4d9c4a2463626aaadc389f
赖王 <2799199459@qq.com>
2022-05-17 09:09:28 +0000

    ! 完成超星门禁进出数据库查询功能 * 添加查询visit_data_log表

--- Mijin.Library.CxData.Query/MainWindow.xaml.cs
+++ Mijin.Library.CxData.Query/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Bing.Extensions;
+using Bing.Extensions.Datas;
+using Mijin.Library.App.Driver;
+using Newtonsoft.Json;
 
 namespace Mijin.Library.CxData.Query;
 /// <summary>
@@ -19,8 +24,106 @@ namespace Mijin.Library.CxData.Query;
 /// </summary>
 public partial class MainWindow : Window
 {
+
+    public class WindowData
+    {
+    }
+
     public MainWindow()
     {
         InitializeComponent();
+
+        // 配置连接字符串
+        SQLHelper.ConnStrs.Remove("cx");
+        SQLHelper.ConnStrs.Add("cx", "Server=192.168.0.81;Database=mj;User=thirdlib;Password=Thirdlib@123;MultipleActiveResultSets=True;");
+        //Insert();
+
+    }
+
+    //查询
+    public DataTable Find(int num)
+    {
+        var data = new DataTable();
+
+        try
+        {
+            data = SQLHelper.QueryDataTable("cx", $"SELECT top {num} * FROM visit_data_log ORDER BY visit_date_time desc", null, CommandType.Text);
+        }
+        catch (Exception e)
+        {
+            MessageBox.Show( e.StackTrace,"查询数据失败");
+        }
+        return data;
+    }
+
+    /// <summary>
+    /// 插入假数据
+    /// </summary>
+    public void Insert()
+    {
+        for (int i = 0; i < 1000; i++)
+        {
+            var date = DateTime.Now.AddMilliseconds( i * 100);
+            CxEntity entity = new CxEntity()
+            {
+                user_id = i.ToString(),
+                direction = "0",
+                sn_name = "0",
+                user_name = "0",
+                card_id = "0",
+
+                visit_date_time = date.ToString("yyyy-MM-dd HH:mm:ss"),
+                visit_date = date.ToString("yyyy-MM-dd"),
+                visit_time = date.ToString("HH:mm:ss"),
+            };
+            entity.WriteToDb();
+
+
+        }
+        
+    }
+
+
+    //设置中文标题
+    public void Change_DataGrid_ColumnName_Ex(DataTable dgv)
+    {
+        dgv.Columns[0].ColumnName = "编号";
+        dgv.Columns[1].ColumnName = "认证记录日期及时间";
+        dgv.Columns[2].ColumnName = "认证记录日期";
+        dgv.Columns[3].ColumnName = "认证记录时间";
+        dgv.Columns[4].ColumnName = "方向";
+        dgv.Columns[5].ColumnName = "设备名称";
+        dgv.Columns[6].ColumnName = "设备序列号";
+        dgv.Columns[7].ColumnName = "人员名称";
+        dgv.Columns[8].ColumnName = "卡号（军官证号）";
+    }
+
+    private async void QueryBtn_Click(object sender, RoutedEventArgs e)
+    {
+
+        var button =  (sender as Button)!;
+
+        button.Content = "查询中···";
+        button.IsEnabled = false;
+
+        if (Sum.Text.IsEmpty())
+            Sum.Text = "50";
+
+        await Task.Delay(500);
+
+        var data = Find(int.Parse(Sum.Text));
+        Change_DataGrid_ColumnName_Ex(data);
+        Show.ItemsSource = data.DefaultView;
+
+        var dataTable = SQLHelper.QueryDataTable("cx", "select count(user_id) from visit_data_log", null, CommandType.Text);
+        var count = dataTable.Rows[0].ItemArray.First();
+        CountLabel.Content = count?.ToString();
+
+        await Task.Delay(500);
+
+        button.Content = "查询";
+        button.IsEnabled = true;
+
+
     }
 }
